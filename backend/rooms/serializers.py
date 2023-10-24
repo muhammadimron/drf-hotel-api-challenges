d94b8b7d1cb2dceeeca4cb90d544b887f64a2df9
Muhammad Imron <muhammadimron1410@gmail.com>
2023-10-24 09:46:36 +0700

    add validators on rooms

--- backend/rooms/serializers.py
+++ backend/rooms/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from .models import Room
 
@@ -9,6 +10,14 @@ class RoomSeliazer(serializers.ModelSerializer):
             'number',
             'floor'
         ]
+
+    def clean(self):
+        floor = self.validated_data.get('floor')
+        number = self.validated_data.get('number')
+        if floor:
+            duplicate = Room.objects.filter(floor=floor, number=number).first()
+            if duplicate:
+                raise ValidationError(f"Room number {floor} in floor {number} has existed")
 
 class RoomDetailSerializer(serializers.Serializer):
     number = serializers.IntegerField(read_only=True)
--- backend/rooms/viewsets.py
+++ backend/rooms/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError as DRFValidationError
+from django.core.exceptions import ValidationError
 
 from .models import Room
 from .serializers import RoomSeliazer
@@ -7,6 +9,14 @@ class RoomViewSets(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSeliazer
     lookup_field = 'pk'
+
+    def perform_create(self, serializer):
+        try:
+            serializer.clean()
+            if serializer.is_valid():
+                serializer.save()
+        except ValidationError as e:
+            raise DRFValidationError({'detail': e.messages[0]})
 
 room_viewsets = RoomViewSets.as_view({
     'get': 'list',
